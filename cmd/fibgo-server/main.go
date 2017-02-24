ac886ae774d810e54f65078ce022edd72dfe572a
Nuruddin Ashr <uudashr@gmail.com>
2017-02-24 17:36:23 +0700

    Update the fibgo-server command

--- cmd/fibgo-server/main.go
+++ cmd/fibgo-server/main.go
@@ -1,15 +1,43 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
 	"net/http"
 
 	fib "github.com/uudashr/fibgo"
 )
 
+var usageMsg = `
+Usage: fibgo-server [options]
+ -p, --port <port>    Listen port
+ -h, --help           Show this message
+`
+
+func usage() {
+	fmt.Println(usageMsg)
+}
+
 func main() {
+	var port int
+	var showHelp bool
+
+	flag.IntVar(&port, "port", 8080, "Listen port")
+	flag.IntVar(&port, "p", 8080, "Listen port")
+	flag.BoolVar(&showHelp, "help", false, "Show this message")
+	flag.BoolVar(&showHelp, "h", false, "Show this message")
+
+	flag.Usage = usage
+	flag.Parse()
+
+	if showHelp {
+		flag.Usage()
+		return
+	}
+
 	handler := fib.NewHTTPHandler()
-	log.Println("Listening on port", 8080, "...")
-	err := http.ListenAndServe(":8080", handler)
+	log.Println("Listening on port", port, "...")
+	err := http.ListenAndServe(fmt.Sprintf(":%d", port), handler)
 	log.Println("Stopped err:", err)
 }
