0ca46aae221dc982e29eabdf7b5de25c9878ccbe
Nuruddin Ashr <uudashr@gmail.com>
2017-04-09 01:10:35 +0700

    Add jenkins build script

---
+++ build-jenkins.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+PRJ=`git config --get remote.origin.url | sed 's/^https:\/\///' | sed 's/\.git$//'`
+BASE_DIR=`pwd`
+BUILD_DIR=build
+
+# Run fresh
+rm -rf $BUILD_DIR
+
+# Install gometalinter
+if [ ! -x "$GOPATH/bin/gometalinter" ]; then
+  go get -u github.com/alecthomas/gometalinter && gometalinter --install
+fi
+
+# Copy source file
+mkdir -p $BUILD_DIR/src/$PRJ
+ls -1 | grep -v ^$BUILD_DIR | xargs -I{} cp -pr {} $BUILD_DIR/src/$PRJ/
+
+cd $BUILD_DIR/src/$PRJ
+GOPATH=$BASE_DIR/$BUILD_DIR
+
+go get -v ./...
+gometalinter ./...
+go test -v ./...
+go install -v ./...
